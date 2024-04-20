2dd4d2b52190fa20c821d659216f1701c8cc26fe
Sandeep Kaur <skaur521995@gmail.com>
2024-04-19 21:53:26 -0400

    created home page

--- lib/models/PostData.dart
+++ lib/models/PostData.dart
@@ -22,21 +22,20 @@ class PostData {
       'postDate': postDate,
     };
   }
-
-  PostData.fromJson(Map<String, dynamic> json)
-      : postId = json['postId'] ?? '',
-        postContent = json['postContent'] ?? '',
-        postImage = json['postImage'],
-        userId = json['userId'] ?? '',
-        postDate = json['postDate'] ?? 0;
-
+  factory PostData.fromJson(Map<dynamic, dynamic> json){
+    return PostData( postId : json['postId'] ?? '',
+        postContent : json['postContent'] ?? '',
+        postImage : json['postImage'],
+        userId : json['userId'] ?? '',
+        postDate : json['postDate'] ?? 0);
+  }
   Map<String, dynamic> toJson() {
     return {
-      'postId': postId,
-      'postContent': postContent,
-      'postImage': postImage,
-      'userId': userId,
-      'postDate': postDate,
+    'postId': postId,
+    'postContent': postContent,
+    'postImage': postImage,
+    'userId': userId,
+    'postDate': postDate,
     };
   }
 }
